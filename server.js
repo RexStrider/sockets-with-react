7e3043977146587b58db5941e57b7eaeb337792f
Odysseus <arwaterman@gmail.com>
2019-08-27 15:17:39 -0700

    prepping for deployment

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 require("./connection/sockets");
+
+
+if (process.env.NODE_ENV === "production") {
+    const express = require('express');
+    const path = require('path');
+    const app = express();
+
+    app.use('/static', express.static(path.join(__dirname, 'client/public')));
+
+    app.use(express.static("client/build"));
+    app.get("/*", function(req, res) {
+      res.sendFile(path.join(__dirname, "/client/build/index.html"));
+    });
+}
