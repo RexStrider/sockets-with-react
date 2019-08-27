5c288b561a508ae156e6476851d7091c22d8e719
Odysseus <arwaterman@gmail.com>
2019-08-27 15:54:47 -0700

    prepping for deployment

--- connection/sockets/index.js
+++ connection/sockets/index.js
@@ -1,28 +1,46 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 
-io.on('connection', client => {
-    console.log(`a user is connected: ${client.id}`);
-    
-    client.on('subscribeToTimer', interval => {
-        console.log('client is subscribing to timer with interval', interval);
+module.exports = server => {
 
-        setInterval(() => {
-            client.emit('timer', new Date());
-        }, interval);
+    // temporary value for socket io, we set this after determing if we are in production or development environment
+    let temp;
+
+    if (process.env.NODE_ENV === "production") {
+        temp = require('socket.io').listen(server)
+    } else {
+        const app = require('express')();
+        const server = require('http').createServer(app);
+        temp = require('socket.io')(server);
+    }
+
+    // make sure io is a constant and can't be changed after determing the environment
+    const io = temp;
+
+    io.on('connection', client => {
+        console.log(`a user is connected: ${client.id}`);
+        
+        client.on('subscribeToTimer', interval => {
+            console.log('client is subscribing to timer with interval', interval);
+
+            setInterval(() => {
+                client.emit('timer', new Date());
+            }, interval);
+        });
+
+        client.on('disconnect', function(){
+            console.log(`user disconnected ${client.id}`);
+        });
+
+        client.on('chat message', msg => {
+            console.log(`${client.id}:`, msg);
+            io.emit('chat message', `${client.id}: ${msg}`);
+        });
     });
 
-    client.on('disconnect', function(){
-        console.log(`user disconnected ${client.id}`);
-    });
+    // if we are **NOT** in production
+    if (process.env.NODE_ENV !== "production") {
+        server.listen(8000, () => {
+            console.log('listening on port 8000');
+        });
+    }
 
-    client.on('chat message', msg => {
-        console.log(`${client.id}:`, msg);
-        io.emit('chat message', `${client.id}: ${msg}`);
-    });
-});
-
-server.listen(8000, () => {
-    console.log('listening on port 8000');
-});
+}
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-require("./connection/sockets");
-
-
 if (process.env.NODE_ENV === "production") {
     const express = require('express');
     const path = require('path');
+
     const app = express();
+
+    const PORT = process.env.PORT || 3001;
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
     app.use('/static', express.static(path.join(__dirname, 'client/public')));
 
@@ -12,4 +15,14 @@ if (process.env.NODE_ENV === "production") {
     app.get("/*", function(req, res) {
       res.sendFile(path.join(__dirname, "/client/build/index.html"));
     });
+
+    // Express server listens on PORT specified
+    const server = app.listen(PORT, () => {
+      console.log(`Server is listening`);
+    });
+
+    require("./connection/sockets")(server);
+}
+else {
+  require("./connection/sockets");
 }
